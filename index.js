de2e59545f1bcfac0177c0d2108bfef1fc37a00d
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-27 02:34:25 +0300

    add STATIC methods

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^\w\s]/gi, '')
+  }
+
+  static titleize(string) {
+    return string
+      .split(' ')
+      .map(word => Formatter.capitalize(word))
+      .join(' ')
+  }
+  
 }
